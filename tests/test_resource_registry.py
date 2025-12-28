a11b28867f5a71d75c982b0ac4d5638aadcdc91c
Mark Servilla <mark.servilla@gmail.com>
2025-12-27 19:55:03 -0700

    Summary: Add test_resource_registry scaffolding.

---
+++ tests/test_resource_registry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Summary:
+
+Module:
+    test_resource_registry
+
+Author:
+    servilla
+
+Created:
+    2025-12-28
+"""
+import daiquiri
+import pytest
+
+
+logger = daiquiri.getLogger(__name__)
+
+
+def test_get_from_date_created(resource_registry):
+    """Test retrieval of data package resources created on or after a specific date."""
+    pass
