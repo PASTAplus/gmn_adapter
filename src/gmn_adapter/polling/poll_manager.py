96c9a3d61b4fd05233a4bf198f3174162a06fad5
Mark Servilla <mark.servilla@gmail.com>
2025-12-28 19:02:19 -0700

    Summary: Add poll_manager to read and enqueue new data package resources into the adapter queue.

---
+++ src/gmn_adapter/polling/poll_manager.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Summary: To poll the PASTA data package manager for new resources
+
+Module:
+    poll_manager
+
+Author:
+    servilla
+
+Created:
+    2025-12-28
+"""
+from datetime import datetime
+import logging
+from pathlib import Path
+import sys
+
+import click
+import daiquiri
+
+from gmn_adapter.config import Config
+from gmn_adapter.model.adapter_db import QueueManager
+from gmn_adapter.model.event import Event
+from gmn_adapter.model.resource_registry import ResourceRegistry
+
+# Set up daiquiri logging: INFO and higher to LOGFILE, WARNING and higher to STDERR
+CWD = Path(".").resolve().as_posix()
+LOGFILE = CWD + "/poll_manager.log"
+daiquiri.setup(
+    level=logging.INFO,
+    outputs=(
+        daiquiri.output.Stream(sys.stderr, level=logging.WARNING),
+        daiquiri.output.File(LOGFILE, level=logging.INFO),
+    ),
+)
+logger = daiquiri.getLogger(__name__)
+
+
+def poll_manager(bootstrap: bool, limit: int, timestamp: str, verbose: int):
+    """Poll the PASTA data package manager for new resources."""
+    if bootstrap:
+        timestamp = Config.TIMESTAMP
+        Path(Config.QUEUE).unlink(missing_ok=True)
+
+    queue_manager = QueueManager(Config.QUEUE)
+
+    if not timestamp:
+        timestamp = (queue_manager.get_last_datetime()).isoformat()
+
+    resource_registry = ResourceRegistry()
+    resources = resource_registry.get_from_date_created(timestamp, limit=limit)
+    while resources:
+        for resource in resources:
+            package = resource[0]
+            timestamp = resource[1]
+            doi = resource[2]
+            owner = resource[3]
+            event = Event(package=package, timestamp=timestamp, owner=owner, doi=doi)
+            logger.info(f"Enqueue: {event}")
+            if verbose > 0:
+                print(f"Package: {package}, Timestamp: {timestamp}, DOI: {doi}, Owner: {owner}")
+            queue_manager.enqueue(event)
+        timestamp = (queue_manager.get_last_datetime()).isoformat()
+        resources = resource_registry.get_from_date_created(timestamp, limit=limit)
+    else:
+        logger.info(f"No new resources created after: {timestamp}.")
+        if verbose > 0:
+            print(f"No new resources created after: {timestamp}.")
+
+    return
+
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+help_bootstrap = "Bootstrap the adapter queue database."
+help_limit = "Chunk limit on the number of polled resources per interation (default=100)."
+help_timestamp = "ISO 8601 timestamp to start polling from."
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option("--bootstrap", is_flag=True, help=help_bootstrap)
+@click.option("--limit", type=int, default=100, help=help_limit)
+@click.option("--timestamp",type=str, help=help_timestamp)
+@click.option("-v", "--verbose", count=True)
+def cli(bootstrap: bool, limit: int, timestamp: str, verbose: int):
+    """CLI wrapper for the poll_manager function.\n
+
+    The poll_manager function polls the PASTA data package manager for new resources and
+    enqueues them in the adapter queue database. See below for options.
+
+    """
+    poll_manager(bootstrap, limit, timestamp, verbose)
+
+
+if __name__ == "__main__":
+    cli()
