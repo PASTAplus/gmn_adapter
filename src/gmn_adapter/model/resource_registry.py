46ae24a6a15cd06249abec22a4ceaf8d91513432
Mark Servilla <mark.servilla@gmail.com>
2025-12-29 20:03:09 -0700

    Summary: Add support for specific supported PASTA scope values. Context: Narrow the specific scope values used for each of the GMNs. Change: Add sets of scope valued to be included into the resource_registry query.

--- src/gmn_adapter/model/resource_registry.py
+++ src/gmn_adapter/model/resource_registry.py
@@ -22,20 +22,30 @@ from gmn_adapter.config import Config
 
 logger = daiquiri.getLogger(__name__)
 
-COLUMNS = "package_id, date_created, doi, principal_owner"
-SELECT = (f"SELECT {COLUMNS} FROM datapackagemanager.resource_registry WHERE CLAUSE AND resource_type = 'dataPackage' AND "
-          f"scope LIKE '{Config.GMN_SCOPE}%' ORDER BY date_created LIMIT;")
 
 class ResourceRegistry:
 
-    def __init__(self):
+    def __init__(self, scope: str):
+
+        if scope == "EDI":
+            self.scope = Config.EDI_SCOPE
+        elif scope == "LTER":
+            self.scope = Config.LTER_SCOPE
+        else:
+            raise ValueError(f"Invalid scope: {scope}")
+
         conn_str = (f"{Config.DB_DRIVER}://"
                     f"{Config.DB_USER}:{urllib.parse.quote_plus(Config.DB_PW)}@"
                     f"{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB}")
         self._engine = create_engine(conn_str)
 
     def get_from_date_created(self, date_created, limit=None) -> Sequence[Row[Any]]:
-        sql = SELECT.replace("CLAUSE", f"date_created >'{date_created}'")
+        columns = "package_id, date_created, doi, principal_owner"
+        select = (f"SELECT {columns} FROM datapackagemanager.resource_registry "
+                  f"WHERE CLAUSE AND resource_type = 'dataPackage' AND "
+                  f"scope IN {self.scope} "
+                  f"ORDER BY date_created LIMIT;")
+        sql = select.replace("CLAUSE", f"date_created >'{date_created}'")
         if limit is not None:
             sql = sql.replace("LIMIT;", f"LIMIT {limit};")
         else:
