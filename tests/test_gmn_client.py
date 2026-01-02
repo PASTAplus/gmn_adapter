73198b595911dbddc0e7bf6b2dd780764cf6b866
Mark Servilla <mark.servilla@gmail.com>
2026-01-01 20:39:19 -0700

    Summary: Add mime_type and replica tests.

--- tests/test_gmn_client.py
+++ tests/test_gmn_client.py
@@ -12,12 +12,15 @@ Author:
 Created:
     2025-12-29
 """
-import pytest
+from datetime import datetime
 
+import pytest
 import daiquiri
 import d1_common.types.exceptions
 
 from gmn_adapter.gmn.client import Client
+from gmn_adapter.models.dataone.replica import Replica, ReplicaStatus
+from gmn_adapter.models.mime.mime_types import MimeType
 
 
 logger = daiquiri.getLogger(__name__)
@@ -41,3 +44,34 @@ def test_list_objects():
     assert objects is not None
     for info in objects.objectInfo:
         print(info.identifier.value())
+
+
+def test_mime_type():
+    mt = MimeType()
+    assert mt is not None
+    mime_type = mt.get_mime_type("xml")
+    assert mime_type == "application/xml"
+    assert not mt.is_valid("eml")
+
+
+def test_replica():
+    replica = Replica(
+        replica_member_node="urn:node:EDI",
+        replication_status=ReplicaStatus.QUEUED,
+        replication_verified=datetime.fromisoformat("2025-12-29 12:00:00")
+    )
+    assert replica is not None
+
+    replica = Replica(
+        replica_member_node="urn:node:EDI",
+        replication_status="queued",
+        replication_verified="2025-12-29 12:00:00"
+    )
+    assert replica is not None
+
+    with pytest.raises(TypeError):
+        Replica(
+            replica_member_node="urn:node:EDI",
+            replication_status="bad_input",
+            replication_verified="2025-12-29 12:00:00"
+        )
