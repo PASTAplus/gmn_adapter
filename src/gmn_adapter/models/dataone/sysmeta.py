231bf3c9e47fb28f1b2b9782ef775ab9b4b7e3e6
Mark Servilla <mark.servilla@gmail.com>
2026-01-01 20:38:47 -0700

    Summary: Add v2 attributes; sysmeta optional attributes; Repica class.

--- src/gmn_adapter/models/dataone/sysmeta.py
+++ src/gmn_adapter/models/dataone/sysmeta.py
@@ -18,25 +18,32 @@ from datetime import datetime
 
 import daiquiri
 
+from gmn_adapter.models.dataone.access_policy import AccessPolicy
+from gmn_adapter.models.dataone.replica import Replica
+from gmn_adapter.models.dataone.replication_policy import ReplicationPolicy
+
 
 logger = daiquiri.getLogger(__name__)
 
 @dataclass
 class SysMeta:
-    serial_version: int
+    serial_version: int | None
     identifier: str
     format_id: str
     size: int
     checksum: str
-    submitter: str
+    submitter: str | None
     rights_holder: str
-    access_policy: list
-    replication_policy: list
-    obsoletes: str
-    obsoleted_by: str
-    archived: bool
-    date_uploaded: datetime
-    origin_member_node: str
-    authoritative_member_node: str
-    replica: list
+    access_policy: AccessPolicy | None
+    replication_policy: ReplicationPolicy | None
+    obsoletes: str | None
+    obsoleted_by: str | None
+    archived: bool | None
+    date_uploaded: datetime | None
+    origin_member_node: str | None
+    authoritative_member_node: str | None
+    replica: Replica | None
+    series_id: str | None
+    media_type: str | None
+    file_name: str | None
 
