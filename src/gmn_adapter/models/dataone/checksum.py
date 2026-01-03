4868ca70aafaa98050bb44ba541d42db305448c0
Mark Servilla <mark.servilla@gmail.com>
2026-01-03 15:53:37 -0700

    Summary: Add checksum model.

---
+++ src/gmn_adapter/models/dataone/checksum.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Summary:
+
+Module:
+    checksum
+
+Author:
+    servilla
+
+Created:
+    2026-01-03
+"""
+import daiquiri
+from pydantic import BaseModel, ConfigDict, Field
+
+logger = daiquiri.getLogger(__name__)
+
+
+class Checksum(BaseModel):
+    """
+    DataONE checksum model.
+
+    Attributes:
+        checksum (str): Checksum value
+        algorithm (str): Checksum algorithm (default: "MD5")
+    """
+    checksum: str
+    algorithm: str = Field(default="MD5")
