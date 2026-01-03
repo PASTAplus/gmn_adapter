f1bab2b3f39e494e35ac164ad52f836b7d0df28f
Mark Servilla <mark.servilla@gmail.com>
2026-01-03 15:56:08 -0700

    Summary: Update test_get_system_metadata; move model tests to separate test_gmn_models.

--- tests/test_gmn_client.py
+++ tests/test_gmn_client.py
@@ -12,18 +12,9 @@ Author:
 Created:
     2025-12-29
 """
-from datetime import datetime
-
-from pydantic import ValidationError
-import pytest
 import daiquiri
 
 from gmn_adapter.gmn.client import Client
-from gmn_adapter.models.dataone.access_policy import AccessPolicy, Allow, AccessRule, Permission
-from gmn_adapter.models.dataone.replica import Replica, ReplicaStatus
-from gmn_adapter.models.dataone.replication_policy import ReplicationPolicy
-from gmn_adapter.models.dataone.sysmeta import SysMeta
-from gmn_adapter.models.mime.mime_types import MimeType
 
 
 logger = daiquiri.getLogger(__name__)
@@ -35,10 +26,12 @@ def test_gmn_client():
 
 
 def test_get_system_metadata():
-    pid = "https://pasta.lternet.edu/package/metadata/eml/edi/2033/2"
+    pid = "https://pasta.lternet.edu/package/metadata/eml/edi/2033/1"
     client = Client("EDI")
-    metadata = client.get_system_metadata(pid)
-    assert metadata is not None
+    sys_meta = client.get_system_metadata(pid)
+    assert sys_meta is not None
+    print("\n")
+    print(sys_meta.model_dump_json(indent=4, exclude_none=True))
 
 
 def test_list_objects():
@@ -47,145 +40,3 @@ def test_list_objects():
     assert objects is not None
     for info in objects.objectInfo:
         print(info.identifier.value())
-
-
-def test_mime_type():
-    mt = MimeType()
-    assert mt is not None
-    mime_type = mt.get_mime_type("xml")
-    assert mime_type == "application/xml"
-    assert not mt.is_valid("eml")
-
-
-def test_replica():
-    replica = Replica(
-        replica_member_node="urn:node:EDI",
-        replication_status=ReplicaStatus.QUEUED,
-        replication_verified=datetime.fromisoformat("2025-12-29 12:00:00")
-    )
-    assert replica is not None
-
-    replica = Replica(
-        replica_member_node="urn:node:EDI",
-        replication_status="queued",
-        replication_verified="2025-12-29 12:00:00"
-    )
-    assert replica is not None
-
-    with pytest.raises(ValidationError):
-        Replica(
-            replica_member_node="urn:node:EDI",
-            replication_status="bad_input",
-            replication_verified="2025-12-29 12:00:00"
-        )
-
-
-def test_replication_policy():
-    replication_policy = ReplicationPolicy(
-        preferred_member_node=["urn:node:EDI"],
-        blocked_member_node=["urn:node:LTER"],
-        replication_allowed=True,
-        number_replicas=1
-    )
-    assert replication_policy is not None
-
-    with pytest.raises(ValidationError):
-        ReplicationPolicy(
-            preferred_member_node=["urn:node:EDI", 4],
-            blocked_member_node=["urn:node:LTER"],
-            replication_allowed=True,
-            number_replicas=1
-        )
-
-
-def test_access_policy():
-    # Test complete access policy
-    access_rule = AccessRule(
-        subject=["EDI-62b5844b7c2dcfd36c37c41b13f4b037c917a901"],
-        permission=[Permission.CHANGE_PERMISSION]
-    )
-    assert access_rule is not None
-
-    allow = Allow(
-        access_rule=[access_rule]
-    )
-    assert allow is not None
-
-    access_policy = AccessPolicy(
-        allow=[allow]
-    )
-    assert access_policy is not None
-
-    # Test empty access policy
-    access_policy = AccessPolicy(
-        allow=[]
-    )
-    assert access_policy is not None
-
-    # Test invalid access policy
-    with pytest.raises(ValidationError):
-        access_rule = AccessRule(
-            subject=["EDI-62b5844b7c2dcfd36c37c41b13f4b037c917a901"],
-            permission=["execute"]
-        )
-        assert access_rule is not None
-
-        allow = Allow(
-            access_rule=[access_rule]
-        )
-        assert allow is not None
-
-        access_policy = AccessPolicy(
-            allow=[allow]
-        )
-
-
-def test_sysmeta():
-    sysmeta = SysMeta(
-        serial_version=1,
-        identifier="https://pasta.lternet.edu/package/metadata/eml/edi/2033/2",
-        format_id="eml://ecoinformatics.org/eml-2.2.0",
-        size=1000,
-        checksum="SHA-1:1234567890abcdef1234567890abcdef12345678",
-        submitter="EDI-62b5844b7c2dcfd36c37c41b13f4b037c917a901",
-        rights_holder="CN=PASTA-GMN,O=EDI,ST=New Mexico,C=US",
-    )
-    assert sysmeta is not None
-
-    access_rule = AccessRule(
-        subject=["EDI-62b5844b7c2dcfd36c37c41b13f4b037c917a901"],
-        permission=[Permission.CHANGE_PERMISSION]
-    )
-    allow = Allow(
-        access_rule=[access_rule]
-    )
-    access_policy = AccessPolicy(
-        allow=[allow]
-    )
-
-    replication_policy = ReplicationPolicy(
-        preferred_member_node=["urn:node:EDI"],
-        blocked_member_node=["urn:node:LTER"],
-        replication_allowed=True,
-        number_replicas=1
-    )
-
-    replica = Replica(
-        replica_member_node="urn:node:EDI",
-        replication_status=ReplicaStatus.QUEUED,
-        replication_verified=datetime.fromisoformat("2025-12-29 12:00:00")
-    )
-
-    sysmeta = SysMeta(
-        serial_version=1,
-        identifier="https://pasta.lternet.edu/package/metadata/eml/edi/2033/2",
-        format_id="eml://ecoinformatics.org/eml-2.2.0",
-        size=1000,
-        checksum="SHA-1:1234567890abcdef1234567890abcdef12345678",
-        submitter="EDI-62b5844b7c2dcfd36c37c41b13f4b037c917a901",
-        rights_holder="CN=PASTA-GMN,O=EDI,ST=New Mexico,C=US",
-        access_policy=access_policy,
-        replication_policy=replication_policy,
-        replica=[replica]
-    )
-    assert sysmeta is not None
