85df76b6228570684b37d3dbcf32cb7e73efdb7f
Mark Servilla <mark.servilla@gmail.com>
2026-01-02 20:37:01 -0700

    Summary: Add tests for DataONE models.

--- tests/test_gmn_client.py
+++ tests/test_gmn_client.py
@@ -14,12 +14,15 @@ Created:
 """
 from datetime import datetime
 
+from pydantic import ValidationError
 import pytest
 import daiquiri
-import d1_common.types.exceptions
 
 from gmn_adapter.gmn.client import Client
+from gmn_adapter.models.dataone.access_policy import AccessPolicy, Allow, AccessRule, Permission
 from gmn_adapter.models.dataone.replica import Replica, ReplicaStatus
+from gmn_adapter.models.dataone.replication_policy import ReplicationPolicy
+from gmn_adapter.models.dataone.sysmeta import SysMeta
 from gmn_adapter.models.mime.mime_types import MimeType
 
 
@@ -69,9 +72,120 @@ def test_replica():
     )
     assert replica is not None
 
-    with pytest.raises(TypeError):
+    with pytest.raises(ValidationError):
         Replica(
             replica_member_node="urn:node:EDI",
             replication_status="bad_input",
             replication_verified="2025-12-29 12:00:00"
         )
+
+
+def test_replication_policy():
+    replication_policy = ReplicationPolicy(
+        preferred_member_node=["urn:node:EDI"],
+        blocked_member_node=["urn:node:LTER"],
+        replication_allowed=True,
+        number_replicas=1
+    )
+    assert replication_policy is not None
+
+    with pytest.raises(ValidationError):
+        ReplicationPolicy(
+            preferred_member_node=["urn:node:EDI", 4],
+            blocked_member_node=["urn:node:LTER"],
+            replication_allowed=True,
+            number_replicas=1
+        )
+
+
+def test_access_policy():
+    # Test complete access policy
+    access_rule = AccessRule(
+        subject=["EDI-62b5844b7c2dcfd36c37c41b13f4b037c917a901"],
+        permission=[Permission.CHANGE_PERMISSION]
+    )
+    assert access_rule is not None
+
+    allow = Allow(
+        access_rule=[access_rule]
+    )
+    assert allow is not None
+
+    access_policy = AccessPolicy(
+        allow=[allow]
+    )
+    assert access_policy is not None
+
+    # Test empty access policy
+    access_policy = AccessPolicy(
+        allow=[]
+    )
+    assert access_policy is not None
+
+    # Test invalid access policy
+    with pytest.raises(ValidationError):
+        access_rule = AccessRule(
+            subject=["EDI-62b5844b7c2dcfd36c37c41b13f4b037c917a901"],
+            permission=["execute"]
+        )
+        assert access_rule is not None
+
+        allow = Allow(
+            access_rule=[access_rule]
+        )
+        assert allow is not None
+
+        access_policy = AccessPolicy(
+            allow=[allow]
+        )
+
+
+def test_sysmeta():
+    sysmeta = SysMeta(
+        serial_version=1,
+        identifier="https://pasta.lternet.edu/package/metadata/eml/edi/2033/2",
+        format_id="eml://ecoinformatics.org/eml-2.2.0",
+        size=1000,
+        checksum="SHA-1:1234567890abcdef1234567890abcdef12345678",
+        submitter="EDI-62b5844b7c2dcfd36c37c41b13f4b037c917a901",
+        rights_holder="CN=PASTA-GMN,O=EDI,ST=New Mexico,C=US",
+    )
+    assert sysmeta is not None
+
+    access_rule = AccessRule(
+        subject=["EDI-62b5844b7c2dcfd36c37c41b13f4b037c917a901"],
+        permission=[Permission.CHANGE_PERMISSION]
+    )
+    allow = Allow(
+        access_rule=[access_rule]
+    )
+    access_policy = AccessPolicy(
+        allow=[allow]
+    )
+
+    replication_policy = ReplicationPolicy(
+        preferred_member_node=["urn:node:EDI"],
+        blocked_member_node=["urn:node:LTER"],
+        replication_allowed=True,
+        number_replicas=1
+    )
+
+    replica = Replica(
+        replica_member_node="urn:node:EDI",
+        replication_status=ReplicaStatus.QUEUED,
+        replication_verified=datetime.fromisoformat("2025-12-29 12:00:00")
+    )
+
+    sysmeta = SysMeta(
+        serial_version=1,
+        identifier="https://pasta.lternet.edu/package/metadata/eml/edi/2033/2",
+        format_id="eml://ecoinformatics.org/eml-2.2.0",
+        size=1000,
+        checksum="SHA-1:1234567890abcdef1234567890abcdef12345678",
+        submitter="EDI-62b5844b7c2dcfd36c37c41b13f4b037c917a901",
+        rights_holder="CN=PASTA-GMN,O=EDI,ST=New Mexico,C=US",
+        access_policy=access_policy,
+        replication_policy=replication_policy,
+        replica=[replica]
+    )
+    assert sysmeta is not None
