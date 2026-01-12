30e3ebb3527f9647c2db663dcd1b7ac633f3ff21
Mark Servilla <mark.servilla@gmail.com>
2026-01-11 20:29:54 -0700

    Summary: Add test_get_resource_ids(); reformat docstrings.

--- tests/test_resource_registry.py
+++ tests/test_resource_registry.py
@@ -14,15 +14,30 @@ Created:
 """
 import daiquiri
 
+from gmn_adapter.models.pasta.resource_registry import ResourceRegistry
+
 
 logger = daiquiri.getLogger(__name__)
 
 
 def test_get_from_date_created(resource_registry):
-    """Test retrieval of data package resources created on or after a specific date."""
-    resources = resource_registry.get_from_date_created("2025-01-01 12:00:00")
+    """
+    Test retrieval of data package resources created on or after a specific date.
+    """
+    resources = resource_registry.get_from_date_created(scope="EDI", date_created="2025-01-01 12:00:00")
     assert len(resources) >= 1
 
     # Test with limit
-    resources = resource_registry.get_from_date_created("2025-01-01 12:00:00", limit=5)
+    resources = resource_registry.get_from_date_created(scope="EDI", date_created="2025-01-01 12:00:00", limit=5)
     assert len(resources) == 5
+
+
+def test_get_resource_ids(resource_registry):
+    """
+    Test retrieval of data package resource IDs.
+    """
+    resource_ids = resource_registry.get_resource_ids(scope="edi", identifier="1", revision="1")
+    assert len(resource_ids) >= 1
+    print("\n")
+    for resource_id in resource_ids:
+        print(resource_id)
