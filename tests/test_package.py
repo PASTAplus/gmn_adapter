c3f67bede19b7b4ebeb4b3988830042f49424a2b
Mark Servilla <mark.servilla@gmail.com>
2026-01-11 20:30:20 -0700

    Summary: Add test_package module.

---
+++ tests/test_package.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Summary:
+
+Module:
+    test_package
+
+Author:
+    servilla
+
+Created:
+    2026-01-11
+"""
+import daiquiri
+
+from gmn_adapter.models.pasta.package import Package
+
+
+logger = daiquiri.getLogger(__name__)
+
+
+def test_package():
+    package = Package("knb-lter-nin.1.1")
+    assert package is not None
+
+    print("\n")
+    print(f"doi: {package.doi}")
+    print(f"date_deactivated: {package.date_deactivated}")
+    for key, value in package.resource_ids.items():
+        print(f"{key}: {value}")
+    print(f"is_gmn_candidate: {package.is_gmn_candidate}")
