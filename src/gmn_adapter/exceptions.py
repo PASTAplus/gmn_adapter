1e4646eda98d0e358f4e85f2ce2c8013832ecc08
Mark Servilla <mark.servilla@gmail.com>
2026-01-11 20:22:20 -0700

    Summary: Add project exceptions.

---
+++ src/gmn_adapter/exceptions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Summary: GMN adapter exceptions.
+
+Module:
+    exceptions
+
+Author:
+    servilla
+
+Created:
+    2026-01-11
+"""
+import daiquiri
+
+
+logger = daiquiri.getLogger(__name__)
+
+
+class GMNAdapterError(Exception):
+    pass
+
+
+class GMNAdapterDataPackageResourcesNotFound(GMNAdapterError):
+    pass
+
+
+class GMNAdapterDataPackageNotFound(GMNAdapterError):
+    pass
