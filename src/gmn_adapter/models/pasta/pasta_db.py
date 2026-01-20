253fdd380d8c6d78c1d21e9b2c571a754857ef16
Mark Servilla <mark.servilla@gmail.com>
2026-01-20 15:01:27 -0700

    Summary: Parameterize function signature to allow for in-code customization.

--- src/gmn_adapter/models/pasta/pasta_db.py
+++ src/gmn_adapter/models/pasta/pasta_db.py
@@ -13,7 +13,7 @@ Author:
 Created:
     2026-01-19
 """
-import urllib
+import urllib.parse
 
 import daiquiri
 from sqlalchemy import create_engine, Engine
@@ -23,14 +23,27 @@ from gmn_adapter.config import Config
 logger = daiquiri.getLogger(__name__)
 
 
-def get_pasta_db_engine() -> Engine:
+def get_pasta_db_engine(
+    db_driver: str=Config.DB_DRIVER,
+    db_user: str=Config.DB_USER,
+    db_pw: str=Config.DB_PW,
+    host: str=Config.DB_HOST,
+    port: str=Config.DB_PORT,
+    db: str=Config.DB
+) -> Engine:
     """
     Creates and returns a database engine for the PASTA database.
+
+    Args:
+        db_driver (str, optional): Database driver. Defaults to Config.DB_DRIVER.
+        db_user (str, optional): Database user. Defaults to Config.DB_USER.
+        db_pw (str, optional): Database password. Defaults to Config.DB_PW.
+        host (str, optional): Database host. Defaults to Config.DB_HOST.
+        port (str, optional): Database port. Defaults to Config.DB_PORT.
+        db (str, optional): Database name. Defaults to Config.DB.
 
     Returns:
         Engine: A SQLAlchemy Engine instance connected to the specified PASTA database.
     """
-    conn_str = (f"{Config.DB_DRIVER}://"
-                f"{Config.DB_USER}:{urllib.parse.quote_plus(Config.DB_PW)}@"
-                f"{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB}")
+    conn_str = f"{db_driver}://{db_user}:{urllib.parse.quote_plus(db_pw)}@{host}:{port}/{db}"
     return create_engine(conn_str)
