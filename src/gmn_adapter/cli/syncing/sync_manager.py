74829b563d32bc423cb362999fb55343c50f58b3
Mark Servilla <mark.servilla@gmail.com>
2026-02-17 20:41:21 -0700

    Summary: Remove lockfile path option; replace print with click.echo statements; replace exit() with sys.exit(); add sync_irq to safely interrupt loop.

--- src/gmn_adapter/cli/syncing/sync_manager.py
+++ src/gmn_adapter/cli/syncing/sync_manager.py
@@ -22,17 +22,16 @@ import daiquiri
 
 from gmn_adapter.config import Config
 from gmn_adapter.cli.configuration import configuration
+from gmn_adapter.cli.synchronize import synchronize_to_gmn
+from gmn_adapter.exceptions import GMNAdapterDataPackageExists, GMNAdapterPartialDataPackageExists, GMNAdapterNonSynchronizedAncestor
 from gmn_adapter.lock import Lock
-from gmn_adapter.exceptions import GMNAdapterDataPackageExists, GMNAdapterPartialDataPackageExists, \
-    GMNAdapterNonSynchronizedAncestor
 from gmn_adapter.models.adapter.adapter_db import QueueManager
 from gmn_adapter.models.pasta.package import Package
 from gmn_adapter.models.pasta.pasta_db import get_pasta_db_engine
-from gmn_adapter.cli.synchronize import synchronize_to_gmn
 
 
 # Set up daiquiri logging: INFO and higher to LOGFILE, WARNING and higher to STDERR
-LOGFILE = Config.LOGS_DIR / f"{Path(__file__).stem}.log"
+LOGFILE = Config.LOGS_DIR / Path(__file__).with_suffix(".log").name
 daiquiri.setup(
     level=logging.INFO,
     outputs=(
@@ -43,85 +42,101 @@ daiquiri.setup(
 logger = daiquiri.getLogger(__name__)
 
 
-def sync_manager(lock_file: str, verbose: int) -> int:
+def sync_manager(dryrun: bool, verbose: int) -> int:
     """Manage synchronization tasks to the GMN."""
 
-    lock = Lock(lock_file)
+    lock = Lock(Config.SYNC_LOCK)
     if lock.locked:
-        logger.error('Lock file {} exists, exiting...'.format(lock.lock_file))
-        return 1
+        logger.error(f"Lock file {lock.lock_file} exists, exiting...")
+        sys.exit(1)
     else:
         lock.acquire()
-        logger.warning('Lock file {} acquired'.format(lock.lock_file))
+        logger.warning(f"Lock file {lock.lock_file} acquired")
 
     pasta_db_engine = get_pasta_db_engine()
     queue_manager = QueueManager(Config.QUEUE)
     queue_manager.set_all_clean()  # Mark all queued packages as clean for GMN synchronization inspection.
     queue_head = queue_manager.get_head(clean=True)
+
     while queue_head:
+
+        # Allow for controlled exit
+        if Config.SYNC_IRQ.exists():
+            click.echo("Sync IRQ detected, exiting...")
+            logger.info("Sync IRQ detected, exiting...")
+            break
+
         pid = str(queue_head.package)
-        package = Package(pid=pid, pasta_db_engine=pasta_db_engine)
         if verbose > 0:
-            print(f"Attempting to synchronize package: {package.pid}")
+            click.echo(f"Processing package: {pid}")
+        logger.info(f"Processing package: {pid}")
+        package = Package(pid=pid, pasta_db_engine=pasta_db_engine)
+
+        if verbose > 0:
+            click.echo(f"Attempting to synchronize package: {package.pid}")
         logger.info(f"Attempting to synchronize package: {package.pid}")
         if package.is_gmn_candidate:
             try:
-                synchronize_to_gmn(package=package, queue_manager=queue_manager, pasta_db_engine=pasta_db_engine)
+                synchronize_to_gmn(package=package, queue_manager=queue_manager, pasta_db_engine=pasta_db_engine, dryrun=dryrun)
             except GMNAdapterNonSynchronizedAncestor as e:
                 if verbose > 0:
-                    print(f"Error synchronizing package {package.pid}")
+                    click.echo(f"Error synchronizing package {package.pid}")
                 logger.error(f"Error synchronizing package {package.pid}: {e}")
                 break  # An exceptional Exception has occurred
             except GMNAdapterPartialDataPackageExists as e:
                 if verbose > 0:
-                    print(f"Missing data package resources in GMN for {package.pid}:")
-                    for resource in e.missing_resources: print(f"\t{resource}")
+                    click.echo(f"Missing data package resources in GMN for {package.pid}:")
+                    for resource in e.missing_resources: click.echo(f"\t{resource}")
                 logger.error(f"Error synchronizing package {package.pid}: {e}")
                 break
             except GMNAdapterDataPackageExists as e:
-                # Log message and continue loop
+                # Log the message and continue the loop
                 if verbose > 0:
-                    print(f"Package {package.pid} already exists in GMN.")
+                    click.echo(f"Package {package.pid} already exists in GMN.")
                 logger.info(f"Package {package.pid} already exists in GMN.")
-                queue_manager.dequeue(package.pid)
+                if not dryrun:
+                    queue_manager.dequeue(package.pid)
+                queue_manager.set_dirty(package=pid)
             else:
                 if verbose > 0:
-                    print(f"Package {package.pid} successfully synchronized to GMN.")
+                    click.echo(f"Package {package.pid} successfully synchronized to GMN.")
                 logger.info(f"Package {package.pid} successfully synchronized to GMN.")
-                queue_manager.dequeue(package.pid)
+                if not dryrun:
+                    queue_manager.dequeue(package.pid)
         else:
             if verbose > 0:
-                print(f"Package {package.pid} is not a GMN candidate - skipping.")
+                click.echo(f"Package {package.pid} is not a GMN candidate - skipping.")
             logger.warning(f"Package {package.pid} is not a GMN candidate - skipping.")
             queue_manager.set_dirty(package=pid)
 
         queue_head = queue_manager.get_head(clean=True)
 
     lock.release()
-    logger.warning('Lock file {} released'.format(lock.lock_file))
+    logger.warning(f"Lock file {lock.lock_file} released")
 
-    return 0
+    sys.exit(0)
 
 
 def print_version(ctx: click.Context, param: click.Parameter, value: bool) -> None:
     if not value or ctx.resilient_parsing:
         return
-    print(f"Version: {Config.VERSION.read_text("utf-8")}")
+    click.echo(f"Version: {Config.VERSION.read_text("utf-8")}")
     ctx.exit()
 
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 help_conf = "Current configuration settings."
+help_dryrun = "Perform dryrun synchronization of data packages to the GMN."
 help_lock = "Path to lock file (default /tmp/sync_manager.lock)."
 help_verbose = "Send output to standard out (-v or -vv or -vvv for increasing output)."
 help_version = "Output GMN adapter version and exit."
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option("--conf", is_flag=True, default=False, help=help_conf)
-@click.option("-l", "--lock", type=str, default="/tmp/sync_manager.lock", help=help_lock)
+@click.option("--dryrun", is_flag=True, default=False, help=help_dryrun)
 @click.option("-v", "--verbose", count=True, help=help_verbose)
 @click.option("--version", is_flag=True, default=False, callback=print_version, expose_value=False, is_eager=True, help=help_version)
-def cli(conf: bool, lock: str, verbose: int):
+def cli(conf: bool, dryrun: bool, verbose: int):
     """CLI wrapper for the sync_manager function.\n
 
     The sync_manager function manages synchronization of data packages to the GMN based on the
@@ -132,7 +147,7 @@ def cli(conf: bool, lock: str, verbose: int):
         click.echo(json.dumps(configuration(), indent=4))
         sys.exit(0)
     else:
-        status= sync_manager(lock_file=lock, verbose=verbose)
+        status= sync_manager(dryrun=dryrun, verbose=verbose)
         sys.exit(status)
 
 
