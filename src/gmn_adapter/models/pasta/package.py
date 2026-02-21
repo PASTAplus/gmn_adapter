aaf9d87ac3ba6268dac3265e585c73a2a6156e9e
Mark Servilla <mark.servilla@gmail.com>
2026-02-20 21:37:24 -0700

    Summary: Refactor around resource list; create ORE resource and add to resource list.

--- src/gmn_adapter/models/pasta/package.py
+++ src/gmn_adapter/models/pasta/package.py
@@ -12,32 +12,38 @@ Author:
 Created:
     2026-01-08
 """
-from datetime import datetime
+import hashlib
+from datetime import datetime, timezone
 
 import daiquiri
 
 from sqlalchemy import Engine
 
-from gmn_adapter.exceptions import GMNAdapterDataPackageNotFound, GMNAdapterDataPackageResourcesNotFound
+from gmn_adapter.exceptions import (
+    GMNAdapterDataPackageNotFound, GMNAdapterDataPackageResourcesNotFound, GMNAdapterPackageIsNotGMNCandidate
+)
 import gmn_adapter.iam.client as iam_client
 import gmn_adapter.models.dataone.ore as ore
 from gmn_adapter.models.pasta.resource_registry import ResourceRegistry
+from gmn_adapter.models.pasta.resource_map import ResourceMap
 from gmn_adapter.models.pasta.resource_type import ResourceType
 
 
 logger = daiquiri.getLogger(__name__)
 
 
-def _get_package_resources(resource_registry: ResourceRegistry, scope: str, identifier: str, revision: str) -> list:
+def _get_package_resources(resource_registry: ResourceRegistry, scope: str, identifier: str, revision: str) -> list[tuple]:
     """
     Retrieve resources for a PASTA data package.
 
     Args:
-        scope (str): the scope of the package
-        identifier (str): the identifier of the package
-        revision (str): the revision of the package
+        resource_registry (ResourceRegistry): resource registry instance for PASTA
+            datapackagemanager.resource_registry DB model access.
+        scope (str): the scope of the package.
+        identifier (str): the identifier of the package.
+        revision (str): the revision of the package.
 
-    Returns: resources (dict)
+    Returns: resources (list[tuple]): a list of tuples for each resource in the package.
 
     Throws: GMNAdapterDataPackageResourcesNotFound
 
@@ -49,17 +55,52 @@ def _get_package_resources(resource_registry: ResourceRegistry, scope: str, iden
 
     resource_list = []
     for resource in resources:
-        resource_type = ResourceType(resource[0])
-        resource_id = resource[1].strip()
-        doi = resource[2].strip() if resource[2] is not None else None
-        filename = resource[3].strip() if resource[3] is not None else None
-        date_created = resource[4].isoformat()
-        sha1_checksum = resource[5].strip() if resource[5] is not None else None
-        md5_checksum = resource[6].strip() if resource[6] is not None else None
-        format_type = resource[7].strip() if resource[7] is not None else None
-        data_format = resource[8].strip() if resource[8] is not None else None
-        resource_size = resource[9]
-        r = (resource_type, resource_id, doi, filename, date_created, sha1_checksum, md5_checksum, format_type, data_format, resource_size)
+        resource_type = ResourceType(resource[ResourceMap.RESOURCE_TYPE.value])
+        resource_id = resource[ResourceMap.RESOURCE_ID.value].strip()
+        doi = (
+            resource[ResourceMap.DOI.value].strip()
+            if resource[ResourceMap.DOI.value] is not None
+            else None
+        )
+        filename = (
+            resource[ResourceMap.FILENAME.value].strip()
+            if resource[ResourceMap.FILENAME.value] is not None
+            else None
+        )
+        date_created = resource[ResourceMap.DATE_CREATED.value].isoformat()
+        sha1_checksum = (
+            resource[ResourceMap.SHA1_CHECKSUM.value].strip()
+            if resource[ResourceMap.SHA1_CHECKSUM.value] is not None
+            else None
+        )
+        md5_checksum = (
+            resource[ResourceMap.MD5_CHECKSUM.value].strip()
+            if resource[ResourceMap.MD5_CHECKSUM.value] is not None
+            else None
+        )
+        format_type = (
+            resource[ResourceMap.FORMAT_TYPE.value].strip()
+            if resource[ResourceMap.FORMAT_TYPE.value] is not None
+            else None
+        )
+        data_format = (
+            resource[ResourceMap.DATA_FORMAT.value].strip()
+            if resource[ResourceMap.DATA_FORMAT.value] is not None
+            else None
+        )
+        resource_size = resource[ResourceMap.RESOURCE_SIZE.value]
+        r = (
+            resource_type,
+            resource_id,
+            doi,
+            filename,
+            date_created,
+            sha1_checksum,
+            md5_checksum,
+            format_type,
+            data_format,
+            resource_size,
+        )
         resource_list.append(r)
 
     return resource_list
@@ -81,11 +122,7 @@ def _get_resource_types(resources: list) -> dict:
 
 
 def _get_package_doi(resources: list) -> str | None:
-    doi = None
-    for resource in resources:
-        if resource[0] == ResourceType.DATA_PACKAGE:
-            doi = resource[2]
-    return doi
+    return [r[ResourceMap.DOI] for r in resources if r[ResourceMap.RESOURCE_TYPE] == ResourceType.DATA_PACKAGE][0]
 
 
 
@@ -112,32 +149,40 @@ class Package:
 
         Throws: ValueError, GMNAdapterDataPackageNotFound
         """
-
-        # Verify PID format
-        try:
-            self._scope, self._identifier, self._revision = pid.split(".")
-            if len(self._scope) == 0: raise ValueError(f"Scope cannot be empty: {pid}")
-            if len(self._identifier) == 0 or not self._identifier.isdigit():
-                raise ValueError(f"Identifier must be a positive integer: {pid}")
-            if len(self._revision) == 0 or not self._revision.isdigit():
-                raise ValueError(f"Revision must be a positive integer: {pid}")
-        except ValueError as e:
-            msg = f"Invalid PID format: {pid}"
-            logger.error(msg)
-            raise ValueError(msg) from e
-
+        # Resource registry instance for accessing PASTA datapackagemanager.resource_registry DB model
         resource_registry = ResourceRegistry(pasta_db_engine=pasta_db_engine)
+
+        self._scope, self._identifier, self._revision = pid.split(".")
         self._pid = pid
+
+        # Retrieve resources (including resource metadata) for the data package
         try:
-            self._resources = _get_package_resources(resource_registry, self._scope, self._identifier, self._revision)
+            self._resources: list[tuple] = _get_package_resources(resource_registry, self._scope, self._identifier, self._revision)
         except GMNAdapterDataPackageResourcesNotFound as e:
             msg = f"Data package \"{pid}\" was not found on PASTA."
             raise GMNAdapterDataPackageNotFound(msg) from e
         self._resource_types = _get_resource_types(self._resources)
         self._doi = _get_package_doi(self._resources)
         self._date_deactivated = resource_registry.get_date_deactivated(self._scope, self._identifier, self._revision)
-        self._is_gmn_candidate = self._is_gmn_candidate()
-        self._ore = ore.get_ore(pid=self._doi, resources=self._resource_types)
+        if self._is_gmn_candidate():
+            # Generate ORE document for the data package and add to resources
+            self._ore: bytes = ore.get_ore(resources=self._resources)
+            resource = (
+                ResourceType.ORE,
+                self._doi,
+                None,
+                f"{self._pid}-ore.xml",
+                datetime.now(timezone.utc).isoformat(),
+                hashlib.sha1(self._ore).hexdigest(),
+                hashlib.md5(self._ore).hexdigest(),
+                "http://www.openarchives.org/ore/terms",
+                "application/xml",
+                len(self._ore)
+            )
+            self._resources.append(resource)
+        else:
+            msg = f"Package \"{pid}\" is not a GMN candidate."
+            raise GMNAdapterPackageIsNotGMNCandidate(msg)
 
     @property
     def scope(self) -> str:
@@ -156,6 +201,10 @@ class Package:
         return self._pid
 
     @property
+    def resources(self) -> list:
+        return self._resources
+
+    @property
     def resource_types(self) -> dict:
         return self._resource_types
 
@@ -170,10 +219,6 @@ class Package:
     @property
     def ore(self) -> str:
         return self._ore.decode("utf-8")
-
-    @property
-    def is_gmn_candidate(self) -> bool:
-        return self._is_gmn_candidate
 
     def _is_gmn_candidate(self) -> bool:
         """
@@ -190,28 +235,31 @@ class Package:
             logger.warning(f"Package {self.pid} has been deactivated.")
             return False
 
-        data_entities = self.resource_types[ResourceType.DATA]
-        if len(data_entities) == 0:
+        data_resources = [r for r in self._resources if r[ResourceMap.RESOURCE_TYPE.value] == ResourceType.DATA]
+        if len(data_resources) == 0:
             logger.warning(f"Package {self.pid} has no data entities.")
             return False
 
         public_token = iam_client.get_public_token()
 
-        if not iam_client.is_authorized(public_token, self.resource_types[ResourceType.DATA_PACKAGE], "read"):
+        resource = [r for r in self._resources if r[ResourceMap.RESOURCE_TYPE.value] == ResourceType.DATA_PACKAGE][0]
+        if not iam_client.is_authorized(public_token, resource[ResourceMap.RESOURCE_ID.value], "read"):
             logger.warning(f"Package {self.pid} does not have Public read access to package metadata.")
             return False
 
-        if not iam_client.is_authorized(public_token, self.resource_types[ResourceType.METADATA], "read"):
+        resource = [r for r in self._resources if r[ResourceMap.RESOURCE_TYPE.value] == ResourceType.METADATA][0]
+        if not iam_client.is_authorized(public_token, resource[ResourceMap.RESOURCE_ID.value], "read"):
             logger.warning(f"Package {self.pid} does not have Public read access to package metadata.")
             return False
 
-        if not iam_client.is_authorized(public_token, self.resource_types[ResourceType.REPORT], "read"):
+        resource = [r for r in self._resources if r[ResourceMap.RESOURCE_TYPE.value] == ResourceType.REPORT][0]
+        if not iam_client.is_authorized(public_token, resource[ResourceMap.RESOURCE_ID.value], "read"):
             logger.warning(f"Package {self.pid} does not have Public read access to package report.")
             return False
 
-        for entity in data_entities:
-            if not iam_client.is_authorized(public_token, entity, "read"):
-                logger.warning(f"Package {self.pid} does not have Public read access to data entity {entity}.")
+        for resource in data_resources:
+            if not iam_client.is_authorized(public_token, resource[ResourceMap.RESOURCE_ID.value], "read"):
+                logger.warning(f"Package {self.pid} does not have Public read access to data entity {resource}.")
                 return False
 
         return True
@@ -232,7 +280,6 @@ class Package:
             f"  PID: {self._pid}\n"
             f"  DOI: {self._doi}\n"
             f"  Date Deactivated: {self._date_deactivated}\n"
-            f"  Is GMN Candidate: {self._is_gmn_candidate}\n"
             f"  Resource IDs:\n"
             f"{resources}"
         )
