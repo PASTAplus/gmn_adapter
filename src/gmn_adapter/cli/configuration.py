517263625b84e1b014b41d902447b9174f9d6f71
Mark Servilla <mark.servilla@gmail.com>
2026-02-21 17:14:09 -0700

    Summary: Change node name from EDI or LTER to urn:node:EDI or urn:node:LTER.

--- src/gmn_adapter/cli/configuration.py
+++ src/gmn_adapter/cli/configuration.py
@@ -21,7 +21,7 @@ logger = daiquiri.getLogger(__name__)
 
 
 def configuration() -> dict:
-    gmn_url = Config.GMN_EDI_BASE_URL if Config.GMN_NODE == "EDI" else Config.GMN_LTER_BASE_URL
+    gmn_url = Config.GMN_EDI_BASE_URL if Config.GMN_NODE == Config.GMN_EDI_NODE else Config.GMN_LTER_BASE_URL
     pasta_db = f"{Config.DB_DRIVER}://{Config.DB_USER}:@{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB}"
 
     return {
